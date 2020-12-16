7278246aed5a7c50de9b41378070aa0447c658fe
Martin Jakobsgaard <martin.jakobsgaard@gmail.com>
2020-12-16 16:01:14 +0100

    Final push for working demo.

--- main.py
+++ main.py
@@ -29,11 +29,27 @@ p7 = [0.428629, -0.318319, 0.331533, -2.41957, 1.62123, -0.157895]
 p8 = [0.439175, -0.431889, 0.505297, -2.41965, 1.62118, -0.157964]
 
 
+# Place points
+p9 = [0.439181, -0.431892, 0.505278, -1.70209, 2.41975, -0.269813]
+p10 = [0.439144, -0.556322, 0.505254, -0.702129, 2.94703, -0.352995]
+p11 = [-0.0042988, -0.598356, 0.505273, -0.702218, 2.947, -0.353108]
+
+p12 = [-0.288039, -0.668178, 0.505228, -0.702151, 2.94693, -0.353011]
+p13 = [-0.369101, -0.43335, 0.505187, -0.702024, 2.94685, -0.352988]
+p14 = [-0.309318, -0.403673, 0.278585, -0.702832, 2.95168, -0.33494]
+p15 = [-0.30932, -0.403689, 0.223064, -0.7126, 3.01997, -0.00527375]
+p16 = [-0.477062, -0.0661112, 0.223068, -0.712635, 3.01995, -0.00528498]
+
+p17 = [-0.483899, -0.052367, 0.473396, -0.71254, 3.0199, -0.00532701]
+p18 = [-0.176069, -0.671882, 0.473417, -0.712558, 3.01996, -0.0052031]
+
+
 class gripper_control:
     writer = serial.Serial('/dev/ttyUSB0', 9600)
     rec = rtde_receive.RTDEReceiveInterface(ip)
     control = rtde_control.RTDEControlInterface(ip)
     move_back = False
+    move_forward = False
 
     def __init__(self):
         time.sleep(1)
@@ -50,6 +66,20 @@ class gripper_control:
         self.control.moveL(p7, 0.4, 0.4)
         self.control.moveL(p8, 0.4, 0.4)
 
+        #Place part
+        self.control.moveL(p9, 0.4, 0.4)
+        self.control.moveL(p10, 0.4, 0.4)
+        self.control.moveL(p11, 0.4, 0.4)
+        self.control.moveL(p12, 0.4, 0.4)
+        self.control.moveL(p13, 0.4, 0.4)
+        self.control.moveL(p14, 0.4, 0.4)
+        self.control.moveL(p15, 0.4, 0.4)
+        # Move back place back
+        self.move_forward = True
+        self.control.moveL(p16, 2.5, 0.01)
+        self.control.moveL(p17, 0.4, 0.4)
+        self.control.moveL(p18, 0.4, 0.4)
+
     def gripper_movement(self):
         self.writer.write(struct.pack('>BB', FRONT, 255))
         time.sleep(10)
@@ -60,6 +90,13 @@ class gripper_control:
                 self.move_back = False
                 time.sleep(1.5)
                 self.writer.write(struct.pack('>BB', BACK, 255))
+                time.sleep(10.5)
+            if self.move_forward:
+                time.sleep(1)
+                self.writer.write(struct.pack('>BB', FRONT, 155))
+                self.move_forward = False
+                time.sleep(1.5)
+                self.writer.write(struct.pack('>BB', FRONT, 255))
                 time.sleep(10.5)
                 break
         #print("Done with while true")
